bdd9e1b4f2d1698ba0405bd78e57d2179e2a88f8
odecramer <odecramer@wideagency.com>
2022-11-20 19:49:16 +0100

    Feature #18 - Added new item FileExtractedItem. This is sent after an operation finished reading a file.

--- src/Oliverde8/Component/PhpEtl/ChainOperation/Extract/CsvExtractOperation.php
+++ src/Oliverde8/Component/PhpEtl/ChainOperation/Extract/CsvExtractOperation.php
@@ -7,8 +7,10 @@ use Oliverde8\Component\PhpEtl\ChainOperation\AbstractChainOperation;
 use Oliverde8\Component\PhpEtl\ChainOperation\DataChainOperationInterface;
 use Oliverde8\Component\PhpEtl\Extract\File\Csv;
 use Oliverde8\Component\PhpEtl\Item\DataItemInterface;
+use Oliverde8\Component\PhpEtl\Item\FileExtractedItem;
 use Oliverde8\Component\PhpEtl\Item\GroupedItem;
 use Oliverde8\Component\PhpEtl\Item\ItemInterface;
+use Oliverde8\Component\PhpEtl\Item\MixItem;
 use Oliverde8\Component\PhpEtl\Model\ExecutionContext;
 
 class CsvExtractOperation extends AbstractChainOperation implements DataChainOperationInterface
@@ -38,6 +40,6 @@ class CsvExtractOperation extends AbstractChainOperation implements DataChainOpe
         }
 
         $fileIterator = new Csv($filename, $this->delimiter, $this->enclosure, $this->escape);
-        return new GroupedItem($fileIterator);
+        return new MixItem([new GroupedItem($fileIterator), new FileExtractedItem($filename)]);
     }
 }
