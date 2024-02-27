b43f154c192bd99670a59066763af449f9f81d97
oliverde8 <oliverde8@gmail.com>
2024-02-27 10:22:43 +0100

    Feature #35 - Added ability to follow ETL progress in console.

--- docs/examples/00-describe/01-csv-transform.php
+++ docs/examples/00-describe/01-csv-transform.php
@@ -4,7 +4,13 @@ require_once __DIR__ . "/.init.php";
 
 $chainProcessor = getChainProcessor(__FILE__);
 
+$output = new \Symfony\Component\Console\Output\ConsoleOutput();
+$symfonyOutput = new \Oliverde8\Component\PhpEtl\Output\SymfonyConsoleOutput($output);
+
 $chainProcessor->process(
-    new ArrayIterator([__DIR__ . "/customers.csv"]),
-    []
+    new ArrayIterator([getProcessFilePath(__DIR__, "/customers.csv")]),
+    [],
+    function (array $operationStates) use ($symfonyOutput) {
+        $symfonyOutput->output($operationStates);
+    }
 );
